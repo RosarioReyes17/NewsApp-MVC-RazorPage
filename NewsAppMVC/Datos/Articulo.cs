dd635b154856d258ecafe2f5c6f9447932808bfb
RosarioReyes1717 <rr2018-0340@uce.edu.do>
2021-07-15 20:57:43 -0400

    Index

--- NewsAppMVC/Datos/Articulo.cs
+++ NewsAppMVC/Datos/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,13 +9,20 @@ namespace NewsAppMVC
     public partial class Articulo
     {
         public int IdArticulo { get; set; }
+        [Required(ErrorMessage ="This field is required")]
         public string Autor { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Descripcion { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string ArticuloUrl { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string ImagenUrl { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public DateTime? FechaPublicacion { get; set; }
         public string Contenido { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public int? IdCategoria { get; set; }
         public int? IdPais { get; set; }
         public int? IdFuente { get; set; }
